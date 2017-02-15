54500d9f0f5557b645328b17eeba678f81ba94bf
D. Seager <seager@uk.ibm.com>
2017-02-15 17:03:00 +0000

    Compiled my code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -48,7 +48,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 
 // Invoke is our entry point to invoke a chaincode function
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("invoke is running " + function)
+	fmt.Println("invoke is running goats " + function)
 
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
